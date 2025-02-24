c4c45732d481edf131a79b965489f3f9638278df
Sean <seasushi123@gmail.com>
2025-02-23 22:58:49 -0500

    Update SimpleEncryption.java

--- SimpleEncryption.java
+++ SimpleEncryption.java
@@ -17,25 +17,35 @@ import java.util.Scanner;  // Import the Scanner class
 class SimpleEncryption {
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-        System.out.println("Please enter a sentence: ");
-        String sentence = myObj.nextLine();  
-        sentence = sentence.toLowerCase();// make the word letters all lowercase
+        System.out.println("Please enter a phrase: ");
+        String phrase = myObj.nextLine();  
+        System.out.println("Please enter the rotation amount (1-25): ");
+        Int rotation = myObj.nextInt();  
+        System.out.println("(1)Encryption  (2)Decryption   Pick one: ");
+        Int enOrDe = myObj.nextInt();  
+        phrase = phrase.toUpperCase();// make the word letters all uppercase
 
-
-        String phrase = sentence.replace(" ",""); //remove all of the spaces
-
-        String reversed = ""; // Initialize reversed string for each word
-        //iterate through each letter starting from the end of the phrase and create the reversed phrase
-            for (int i = phrase.length() - 1; i >= 0; i--) {
-              reversed += phrase.charAt(i);
+        String encrypted = "";
+        if (enOrDe == 1 { //if the user chose encryption
+            for (int i = 0; i < phrase.length(); i++) {
+                char current = phrase.charAt(i);
+                if (Character.isLetter(currentChar)) {
+                    char base = 'A'
+                    // Shift the letter by the rotation amount
+                    char newChar = (char) ((current - base + rotation) % 26 + base);
+                    encrypted += newChar;
+                } 
+                else {
+                    encrypted += current // Keep spaces and non-letters unchanged
+                }
             }
-            if (phrase.equals(reversed)) { //if the reversed is the same as the word
-                System.out.println(sentence+" IS a palindrome.");
-                
-            } 
-            else{
-                System.out.println(sentence+" is NOT a palindrome.");
-            }
+            System.out.println("The original phrase is: " + phrase);
+            System.out.println("The encrypted phrase is: " + encrypted);
+
+        } 
+        else{ //if the user chose decryption
+            System.out.println(sentence+" is NOT a palindrome.");
+        }
 
     }
 }
