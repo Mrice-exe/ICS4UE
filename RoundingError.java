ef13b66634d595cf0a195555b2c5116e6e21c002
Sean <seasushi123@gmail.com>
2025-02-23 20:51:40 -0500

    Update RoundingError.java

--- RoundingError.java
+++ RoundingError.java
@@ -8,24 +8,24 @@ RoundingError
 
 Variable dictionary:
     num - stores the int input of user
-    squaroot - the calculated square root of the number
-    square - the square of the square root
-    error - the round off error
+    squaroot - double, the calculated square root of the number
+    square - double, the square of the square root
+    error - double, the round off error
 
 */
 import java.util.Scanner;  // Import the Scanner class
 
 
-class EasterVariation2 {
+class RoundingError {
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
         System.out.println("Rounding Error calculator");
         System.out.println("---------------------------------");
         System.out.println("Please enter a number: ");
         int num = myObj.nextInt();  
-        int squaroot = Math.sqrt(num); //the square root of the number
-        int square = Math.expo(squaroot); //the square of the square root
-        int error = num - square;
+        double squaroot = Math.sqrt(num); //the square root of the number
+        double square = Math.pow(squaroot,2); //the square of the square root
+        double error = num - square; //the round of error calculated from difference
 
         System.out.println("The round of error is: " + error);
 
