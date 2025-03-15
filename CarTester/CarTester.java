5a49b9fe266d21bcdef26c5fd12de39a716aad33
Sean <seasushi123@gmail.com>
2025-03-15 19:45:23 -0400

    Update CarTester.java

--- CarTester/CarTester.java
+++ CarTester/CarTester.java
@@ -1,1 +1,24 @@
+/*
+Sean S
+03/15/2025
+ICS4UE 
+Unit 2: Arrays
+Activity 2: One-Dimensional Arrays
+CarTester
 
+Variable dictionary:
+    nouns - stores the noun Strings into an array
+    verbs - stores the verbs strings into an array
+    rhymingNouns - stores the nouns that rhyme into an array
+    verb1, verb2, verb3 - the first, second, and third randomly chosen verbs for the poem
+    rhymingNoun1, rhymingNoun2, rhymingNoun3 - the first, second, and third randomly chosen rhyming nouns for the poem
+    randomNum - a random integer used for randomly picking nouns or verbs
+    
+*/
+import scanner
+
+class CarTester {
+    public static void main(String[] args) {
+
+  }
+}
