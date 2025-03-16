fae7987a7cea1c1b6d8fe266b79c3e02de5bb230
Sean <seasushi123@gmail.com>
2025-03-16 16:51:22 -0400

    Update CarTester.java

--- CarTester/CarTester.java
+++ CarTester/CarTester.java
@@ -15,10 +15,45 @@ Variable dictionary:
     randomNum - a random integer used for randomly picking nouns or verbs
     
 */
-import scanner
+package CarTester;
+    
+import java.util.Scanner;  // Import the Scanner class
 
 class CarTester {
     public static void main(String[] args) {
+    Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+    System.out.println("Let's create your dream car");
+    System.out.println("What is your desired make?");
+    String make = myObj.nextline();
+    System.out.println("What is your desired model?");
+    String model = myObj.nextline();
+    System.out.println("What is your desired colour?");
+    String colour = myObj.nextline();
+    System.out.println("What is your desired year?");
+    int year = myObj.nextline();
+    System.out.println("What is your desired price?");
+    int price = myObj.nextline();
+    System.out.println("What is your desired horsepower?");
+    int horsepower = myObj.nextline();
+    
+    car car1 = new car();
+    car car2 = new car(make, model, colour, year, price, horsepower);
+    car car3 = new car(make, model, colour);
 
+    //Print the details of each car
+    System.out.println("Here is the default car:");
+    System.out.println(car1.toString());
+    System.out.println("--------------------------");
+
+    System.out.println("Here is your dream car")
+    System.out.println(car2.toString());
+    System.out.println("--------------------------");
+
+    System.out.println("Here is your alternate recommended car");
+    System.out.println(car3);
+    System.out.println("--------------------------");
+
+    //the car honks
+    car.honk();
   }
 }
