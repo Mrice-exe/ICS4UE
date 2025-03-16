de32ca21525ff1d2b423811aa0058b393b5f148e
Sean <seasushi123@gmail.com>
2025-03-15 20:50:19 -0400

    Update and rename Car.java to Car(don'tforget purpose of code).java

---
+++ CarTester/Car(don'tforget purpose of code).java
@@ -0,0 +1,47 @@
+/*
+Sean S
+03/02/2025
+ICS4UE 
+Unit 2: Arrays
+Activity 2: One-Dimensional Arrays
+Triplets
+
+
+Variable dictionary:
+    nouns - stores the noun Strings into an array
+    verbs - stores the verbs strings into an array
+    rhymingNouns - stores the nouns that rhyme into an array
+    verb1, verb2, verb3 - the first, second, and third randomly chosen verbs for the poem
+    rhymingNoun1, rhymingNoun2, rhymingNoun3 - the first, second, and third randomly chosen rhyming nouns for the poem
+    randomNum - a random integer used for randomly picking nouns or verbs
+    
+*/
+package CarTester;
+
+public class Car {
+    //declare variables
+    private String make, model, colour;
+    private Int year, price, horsepower;
+    //consturctors
+    public Car(){
+        make = "Toyota";
+        model = "Rav4";
+        colour = "Black";
+        year = 0;
+        price = 0;
+        horsepower = 0 ;
+    }
+    public Car(String imake, String iToyota, String ){
+        make2 = make
+        model2 = model
+        colour2 = colour
+        year2 = year
+        price2 = price
+        horsepower2 = horsepower
+
+    }
+    public void honk(){
+        System.out.println("HONK!")
+    }
+  }
+}
