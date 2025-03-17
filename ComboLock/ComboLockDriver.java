03f08fb245e862b70ab663e02b8b30886a409398
Sean <seasushi123@gmail.com>
2025-03-16 20:57:10 -0400

    Update ComboLockDriver.java

--- ComboLock/ComboLockDriver.java
+++ ComboLock/ComboLockDriver.java
@@ -1,3 +1,19 @@
+/*
+Sean S
+03/16/2025
+ICS4UE 
+Unit 3: Modular Programming
+Activity 2: Modifying Modules
+ComboLockDriver
+
+The purpose of this code is to imitate cracking open a combination for a lock
+
+Variable dictionary:
+    scanner: A Scanner object used to capture user input from the console.
+    num1, num2, num3: Integer variables that store the user's input when setting the combination for the lock.
+    lock: An object of the ComboLock class, used to create and manage a lock with a combination provided by the user.
+    randomLock: An object of the ComboLock class, used to create a lock with a random combination that the user attempts to guess.
+*/
 import java.util.Scanner;
 
 public class ComboLockDriver {
