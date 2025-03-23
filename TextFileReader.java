4263fa7823e6a18b176dd6f853096d65f62fefa4
Sean <seasushi123@gmail.com>
2025-03-23 19:12:55 -0400

    Create TextFileReader.java

---
+++ TextFileReader.java
@@ -0,0 +1,33 @@
+/*
+Sean S
+03/23/2025
+ICS4UE 
+Unit 3: Modular Programming
+Activity 3: External File Handling 
+TextFileReader
+
+Variable dictionary:
+    num - stores the int input of user
+    squaroot - double, the calculated square root of the number
+    square - double, the square of the square root
+    error - double, the round off error
+
+*/
+import java.util.Scanner;  // Import the Scanner class
+
+
+class TextFileReader {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Rounding Error calculator");
+        System.out.println("---------------------------------");
+        System.out.println("Please enter a number: ");
+        int num = myObj.nextInt();  
+        double squaroot = Math.sqrt(num); //the square root of the number
+        double square = Math.pow(squaroot,2); //the square of the square root
+        double error = num - square; //the round of error calculated from difference
+
+        System.out.println("The round of error is: " + error);
+
+    }
+}
