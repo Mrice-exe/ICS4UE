167c2b34cc87fcf3f339360d557e9c93cc0ef1f2
Sean <seasushi123@gmail.com>
2025-03-30 18:30:14 -0400

    Update NChooseR.java

--- NChooseR.java
+++ NChooseR.java
@@ -8,10 +8,12 @@ NChooseR
 
 Variable dictionary:
     n - stores the desired nth value of the user for fibonacci
-    nth - stores the calcualte number from the sequence
+    r - stores the calcualte number from the sequence
+    total - stores the number of possible ways to choose r objects from a set of n objects
+    ans -  a temporary variable that holds the answer to the formula n!/r!(n-r)!
 */
 import java.util.Scanner;  // Import the Scanner class
-
+ m
 
 class NChooseR {
     public static void main(String[] args) {
@@ -22,24 +24,30 @@ class NChooseR {
         int n = myObj.nextInt();  //take in the input
         System.out.println("Enter the number of different objects chosen from the set");
         int r = myObj.nextInt();  //take in the input
-        int total = combinations(n); //call the function
-        System.out.println("The "+ n + "th fibonacci number is "+nth+"!");
+        int total = combinations(n, r); //call the function
+        System.out.println("There are " + total+" possible ways to choose "+r+" objects from a set of " +n +" objects");
 
     }
-    public static int combinaitons(int n, int r) {
-      if (n < 2){
-        return n; //if n is 1 or 0 then return n
-      }
-      else{
-        return fib(n-1)+ fib(n-2); //recursively call fib(n-1) and add it with a recursive fib(n-2)
-      }
+    public static int combinations(int n, int r) {
+    /* This function calculates the number of ways to choose r different objects from a set of n objects
+    using the formula n!/r!(n-r)!
+    arguments: int n, int r
+    return: int, the answer of the formula with the variables plugged in
+    */
+    //use the formula
+        int ans = fact(n) / (fact(r) * fact(n-r));
+        return ans;
     }
     public static int fact(int x) {
-      if (n < 2){
-        return n; //if n is 1 or 0 then return n
+    /* This function calculates the facotiral of n using recursion
+    arguments: int x
+    return: int, the factorial of x
+    */
+      if (x < 2){
+        return x; //if n is 1 or 0 then return n
       }
       else{
-        return fib(n-1)+ fib(n-2); //recursively call fib(n-1) and add it with a recursive fib(n-2)
+        return x*fact(x-1); //recursively multiply x with the factorial of x-1
       }
     }
 }
